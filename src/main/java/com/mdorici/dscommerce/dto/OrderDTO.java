b6d0a49b045d61e5c484d88012cd21ec568b5b2d
mdorici <marianadorici@gmail.com>
2022-11-17 08:59:53 -0300

    Saving Order

--- src/main/java/com/mdorici/dscommerce/dto/OrderDTO.java
+++ src/main/java/com/mdorici/dscommerce/dto/OrderDTO.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.NotEmpty;
+
 import com.mdorici.dscommerce.entities.Order;
 import com.mdorici.dscommerce.entities.OrderItem;
 import com.mdorici.dscommerce.entities.OrderStatus;
@@ -18,6 +20,7 @@ public class OrderDTO {
 	
 	private PaymentDTO payment;
 	
+	@NotEmpty(message = "Deve ter pelo menos um item.")
 	private List<OrderItemDTO> items = new ArrayList<>();
 
 	public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
